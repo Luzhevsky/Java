edde5c204a0e70a1983cbc21415450b2df7826a4
admin <dmitriy8796@inbox.ru>
2020-04-05 00:12:54 +0300

    Какойто комит

---
+++ kursovaya/src/study/Main.java
@@ -0,0 +1,10 @@
+package study;
+import javax.swing.JFrame;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ kursovaya/src/study/MyWIndowApp.java
@@ -0,0 +1,11 @@
+package study;
+
+import javax.swing.JFrame;
+
+public class MyWIndowApp extends JFrame {
+    public MyWIndowApp(){
+    setBounds(100,100,200,200);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
