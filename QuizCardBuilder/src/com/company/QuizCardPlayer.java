b6290fe595dd9c3787398eb6ae0984c9b1264ec3
admin <dmitriy8796@inbox.ru>
2020-04-12 18:04:34 +0300

    commit

--- QuizCardBuilder/src/com/company/QuizCardPlayer.java
+++ QuizCardBuilder/src/com/company/QuizCardPlayer.java
@@ -4,14 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class QuizCardPlayer {
 
-    JTextArea display;
-    boolean isShowAnser;
+    private JTextArea display;
+    private boolean isShowAnser;
+    private JButton nextButton;
+    private QuizCard currentCard;
+    private int currentCardIndex;
+    private ArrayList<QuizCard> cardList;
+    private JFrame frame;
+    public QuizCardPlayer() {
+    }
+
 
     public void go(){
-        JFrame frame = new JFrame("QuizCardBuilder");
+        frame = new JFrame("QuizCardBuilder");
         JPanel panel = new JPanel();
 
 
@@ -23,6 +36,7 @@ public class QuizCardPlayer {
         menu.add(file);
 
         JMenuItem save = new JMenuItem("Load card set");
+        save.addActionListener(new OpenMenuListener());
         file.add(save);
 
         JMenuItem exit = new JMenuItem("Exit");
@@ -38,11 +52,14 @@ public class QuizCardPlayer {
         qScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         qScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        JButton nextButton = new JButton("Show Question");
-        nextButton.addActionListener(new NextCardListener());
+        nextButton = new JButton("Show Question");
 
         panel.add(qScroll);
         panel.add(nextButton);
+
+        nextButton.addActionListener(new NextCardListener());
+
+
 
 //frame
         frame.setJMenuBar(menu);
@@ -57,13 +74,63 @@ public class QuizCardPlayer {
     public class NextCardListener implements ActionListener {
         public void actionPerformed(ActionEvent event){
             if(isShowAnser){
-
+                display.setText(currentCard.getAnswer());
+                nextButton.setText("Next Card");
+                isShowAnser = false;
             } else {
+                if(currentCardIndex < cardList.size()){
+                    showNextCard();
+                } else {
+                    display.setText("That was last card");
+                    nextButton.setEnabled(false);
+                }
 
             }
         }
 
     }
+    public class OpenMenuListener implements ActionListener {
+        public void actionPerformed(ActionEvent event){
+            JFileChooser f = new JFileChooser();
+            f.showSaveDialog(frame);
+            loadFile(f.getSelectedFile());
+        }
+
+    }
+
+    public void loadFile(File file){
+        cardList = new ArrayList<QuizCard>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while((line = reader.readLine())!= null){
+                makeCard(line);
+            }
+            reader.close();
+        } catch(Exception ex){
+            System.out.println("couldn't read the card file");
+            ex.printStackTrace();
+        }
+    }
+
+    private void makeCard(String lineToParse){
+        String[] result = lineToParse.split("/");
+        QuizCard card = new QuizCard(result[0], result[1]);
+        cardList.add(card);
+        System.out.println("made a card");
+    }
+    private void showNextCard(){
+        currentCard = cardList.get(currentCardIndex);
+        currentCardIndex++;
+        display.setText(currentCard.getQuestion());
+        nextButton.setText("Show Answer");
+        isShowAnser = true;
+    }
+
+
+
+
+
     public static void main(String[] args) {
         // write your code here
 
