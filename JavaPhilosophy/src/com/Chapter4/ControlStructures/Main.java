a116b8eb07184ff8f09c40426506fa48f55697f9
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-03-15 14:34:00 +0300

    ControlStructures 4 Java_philosophy

--- JavaPhilosophy/src/com/Chapter4/ControlStructures/Main.java
+++ JavaPhilosophy/src/com/Chapter4/ControlStructures/Main.java
@@ -1,6 +1,12 @@
 package com.Chapter4.ControlStructures;
 
 public class Main {
+
+    final static String BIN1 ="0110000";
+    final static String BIN2 ="0000110";
+    final static int B1=0x3;
+    final static int B2=0x4;
+
     public static void main(String[] args) {
         System.out.println("helloControlStructures");
         System.out.println("helloworld");
@@ -35,10 +41,10 @@ public class Main {
 //                System.out.println("Равно");
 //        }
 
-        //Задание 4  1 2 3 5 7
-        for (int i = 1; i < 25; i++) {
+        //Задание 4
+        for (int i = 1; i < 10; i++) {
             int p = 0;
-            for (int j = 2; j < 25; j++) {
+            for (int j = 2; j < 10; j++) {
                 if((i % j == 0) && (i != j))
                     p++;
 //                System.out.println(i + "%" + j + " = " + (i % j));
@@ -46,5 +52,24 @@ public class Main {
             if(p == 0)
                 System.out.println(i);
         }
+        //Задание 5.
+
+        System.out.println();
+//        System.out.println(BIN1);
+//        System.out.println(BIN2);
+        System.out.println(Integer.toBinaryString(B1));
+        System.out.println(Integer.toBinaryString(B2));
+        System.out.println(B1);
+        System.out.println(B2);
+        String bs = "";
+        int bb13 = 256;
+        for (int i = 0; i < Integer.SIZE; i++) {
+            int bb14 = bb13%2 == 0 ? 0 : 1;
+            bs = String.valueOf(bb14) + bs;
+            bb13 /= 2;
+        }
+        System.out.println("bs=" + bs);
+
+
     }
 }
