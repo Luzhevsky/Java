affc8d820560a1946b7c3c1c3ed1a13e276fdef5
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-03-16 16:12:49 +0300

    ControlStructures 9 Java_philosophy Fibonacci

--- JavaPhilosophy/src/com/Chapter3/Operators/Fibonacci/FibonacciMain.java
+++ JavaPhilosophy/src/com/Chapter3/Operators/Fibonacci/FibonacciMain.java
@@ -4,19 +4,19 @@ public class FibonacciMain {
         public static void Fibonacci(int f){
             //1 1 2 3 5 8 13 21
             int f0 = 1;
-            int f1 = 1;
-            int f2 = 0;
-            int f3 = 0;
-            for (int i = 0; i < f; i++) {
-                f2 = f2 + f3;//f2=1 f3=1    2)f2=3 f3=1
-                f3 = f3 + f2;
-
-//                f2 = f2 + f1 + f0;// 1 2 3 4 5
-                System.out.println(f1);
-//                f1 = f1 + f0;
+            int f1 = 0;
+            for (int i = 0; i < f; i+=2) {
+                f0 = f0 + f1;// итер 1:  f0=1+1=2; итер2: f0=2+3=5;
+                System.out.print(f0 + " ");
+                if(f%2!=0){
+                   if(i == f-1)break;
+                }
+                f1 = f1 + f0;// иетр 1: f1=1+2=3; итер2: f1=3+5=8;
+                System.out.print(f1 + " ");
             }
+
         }
     public static void main(String[] args) {
-        Fibonacci(24);
+        Fibonacci(11);
     }
 }
