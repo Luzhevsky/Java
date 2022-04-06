0cdc164691e94723f88ba9f9e1137dbe55132c0d
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-04-06 06:24:49 +0300

    AccessControl 8 Packages Java_philosophy

---
+++ JavaPhilosophy/src/com/Chapter6/AccessControl/TaskAccessControl/ConnectionManager.java
@@ -0,0 +1,27 @@
+package com.Chapter6.AccessControl.TaskAccessControl;
+
+class Connection{
+    private static Connection[] massConnection = new Connection[5];
+    private static int j = -1;
+
+    public static Connection instantiation(){
+        j++;
+
+        if (j >= massConnection.length){
+            return null;
+        }
+
+        return massConnection[j] = new Connection();
+    }
+}
+
+public class ConnectionManager {
+    public static void main(String[] args) {
+        //Задание 8.
+        for (int i = 0; i < 8; i++) {
+            System.out.println(Connection.instantiation());
+        }
+
+
+    }
+}
--- JavaPhilosophy/src/com/Testing/Gui/GutTest.java
+++ JavaPhilosophy/src/com/Testing/Gui/GutTest.java
@@ -3,4 +3,6 @@ package com.Testing.Gui;
 import javax.swing.*;
 
 public class GutTest {
+    private JButton buttonStart;
+    private JPanel panelMain;
 }
