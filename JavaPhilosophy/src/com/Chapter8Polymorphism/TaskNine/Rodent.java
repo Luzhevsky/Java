8e2a6ffb3cbba5d01a010cb58d9699033ce5a21a
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-05-10 12:22:05 +0300

    JavaPhilosophy Chapter8Polymorphism task9 Rodent

--- JavaPhilosophy/src/com/Chapter8Polymorphism/TaskNine/Rodent.java
+++ JavaPhilosophy/src/com/Chapter8Polymorphism/TaskNine/Rodent.java
@@ -1,4 +1,88 @@
 package com.Chapter8Polymorphism.TaskNine;
 
+import java.util.Random;
+
+/** Rodent - Грызуны
+ *
+ */
 public class Rodent {
+
+    /** Грызун что-то грызет.
+     *
+     */
+    public void gnaw(){
+        System.out.println("Грызун грызет. (Метод gnow() Класса Rodent).");
+    }
+
+    /** Грызун бежит.
+     *
+     */
+    public void run(){
+        System.out.println("Грызун бежит. (Метод run() Класса Rodent).");
+    }
+}
+
+class Mouse extends Rodent {
+    @Override
+    public void gnaw(){
+        System.out.println("Мышь грызет. (Метод gnow() Класса Mouse).");
+    }
+    @Override
+    public void run(){
+        System.out.println("Мышь бежит. (Метод run() Класса Mouse).");
+    }
+}
+
+class Hamster extends Rodent {
+    @Override
+    public void gnaw(){
+        System.out.println("Хомяк грызет. (Метод gnow() Класса Hamster).");
+    }
+    @Override
+    public void run(){
+        System.out.println("Хомяк бежит. (Метод run() Класса Hamster).");
+    }
+}
+
+class Chinchilla extends Rodent {
+    @Override
+    public void gnaw(){
+        System.out.println("Шиншила грызет. (Метод gnow() Класса Chinchilla).");
+    }
+    @Override
+    public void run(){
+        System.out.println("Шиншила бежит. (Метод run() Класса Chinchilla).");
+    }
+}
+
+class GetRandomHeir {
+    Random r = new Random();
+    public Rodent getR(){
+        switch (r.nextInt(3)){
+            default :
+            case 0: return new Mouse();
+            case 1: return new Hamster();
+            case 2: return new Chinchilla();
+
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        GetRandomHeir getrandom = new GetRandomHeir();
+
+        Rodent[] rodent = new Rodent[5];
+        for (int i = 0; i < rodent.length; i++) {
+            rodent[i] = getrandom.getR();
+        }
+
+
+        for(Rodent r : rodent){
+            r.run();
+            r.gnaw();
+        }
+
+
+    }
 }
