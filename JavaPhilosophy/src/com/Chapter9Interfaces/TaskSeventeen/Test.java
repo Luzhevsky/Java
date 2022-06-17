9f064a1ea57d4ebf3d3a97d2b941c99de960dd99
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-06-17 08:38:46 +0300

    JavaPhilosophy Chapter9Interfaces TaskSeventeen

---
+++ JavaPhilosophy/src/com/Chapter9Interfaces/TaskSeventeen/Test.java
@@ -0,0 +1,11 @@
+package com.Chapter9Interfaces.TaskSeventeen;
+
+public class Test implements Month{
+    public static void main(String[] args) {
+        System.out.println(JANUARY);//Можем использовать не создавая объект значит static
+//        JANUARY = 23; //Cannot assign a value to final variable 'JANUARY'
+
+
+
+    }
+}
