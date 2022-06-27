31712fce90cc104d9a190eab8528e5f651bfc6c1
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-06-27 15:00:18 +0300

    JavaPhilosophy Chapter12InnerClasses taskSeven

---
+++ JavaPhilosophy/src/com/Chapter10InnerClasses/TaskSeven/FieldAndMethod.java
@@ -0,0 +1,29 @@
+package com.Chapter10InnerClasses.TaskSeven;
+
+public class FieldAndMethod {
+    private String field = "FieldOne";
+
+    private void method() {
+        String s = "method()";
+        System.out.println(s);
+        System.out.println(field);
+    }
+
+    public void method2() {
+        PrivateChange p = new PrivateChange();
+        p.privateChange();
+    }
+
+    class PrivateChange {
+        public void privateChange() {
+            field = "FieldTwo";
+            method();
+        }
+    }
+
+    public static void main(String[] args) {
+        FieldAndMethod f = new FieldAndMethod();
+        f.method();
+        f.method2();
+    }
+}
