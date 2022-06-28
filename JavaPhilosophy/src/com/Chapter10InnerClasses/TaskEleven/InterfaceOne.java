5a46cf643664f578d2d98cdb6441f69a15143f57
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-06-28 17:01:47 +0300

    JavaPhilosophy Chapter12InnerClasses TaskEleven

---
+++ JavaPhilosophy/src/com/Chapter10InnerClasses/TaskEleven/InterfaceOne.java
@@ -0,0 +1,32 @@
+package com.Chapter10InnerClasses.TaskEleven;
+
+public interface InterfaceOne {
+    void getName();
+}
+class Outer {
+    String s = "hello";
+    public void method() {
+        System.out.println(s);
+    }
+    
+    public InterfaceOne methodGetLinc() {
+        return new InterfaceOne() {
+            @Override
+            public void getName() {
+                System.out.println("Inner");
+            }
+        };
+    }
+
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        System.out.println(o);
+        o.method();
+        InterfaceOne interfaceOne = o.methodGetLinc();
+        interfaceOne.getName();
+    }
+}
