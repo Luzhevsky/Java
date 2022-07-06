1f28b4b803457f09fa729cbddc486ebf2fe598db
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-07-06 21:46:39 +0300

    JavaPhilosophy Chapter12InnerClasses TaskTwentyTwo

---
+++ JavaPhilosophy/src/com/Chapter10InnerClasses/Test/GreenhouseController/GreenhouseControls.java
@@ -0,0 +1,110 @@
+package com.Chapter10InnerClasses.Test.GreenhouseController;
+
+public class GreenhouseControls extends Controller{
+    private boolean light = false;
+
+    public class LightOn extends Event {
+        public LightOn(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            //код управляющий включением света
+            light = true;
+        }
+    }
+    public String toString() {
+        return "Свет включен.";
+    }
+
+    private boolean water = false;
+    public class WaterOn extends Event {
+        public WaterOn(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            //код управляющий оборудование(водой).
+            water = true;
+        }
+        public  String toString() {
+            return "Полив включен";
+        }
+
+    }
+    public class WaterOff extends Event {
+        public WaterOff(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            //код управляющий оборудование(водой).
+            water = false;
+        }
+        public  String toString() {
+            return "Полив выключен";
+        }
+
+    }
+
+    private String thermostat = "День";
+    public class ThermostatNight extends Event {
+        public ThermostatNight(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // код управления термостатом.
+            thermostat = "Ночь";
+        }
+        public String toString() {
+            return "Термостат использует ночной режим";
+        }
+    }
+    public class ThermostatDay extends Event {
+        public ThermostatDay(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // код управления термостатом.
+            thermostat = "День";
+        }
+        public String toString() {
+            return "Термостат использует дневной режим";
+        }
+    }
+
+    //Пример метода action(), вставляющего новый экземпляр
+    //самого себя в список событий
+    public class Bell extends Event { //Звонок
+        public Bell(long delayTime){
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            addEvent(new Bell(delayTime));
+        }
+        public String toString() {
+            return "ТУууууууу!";
+        }
+    }
+    public class Restart extends Event {
+        private Event[] eventList;
+        public Restart(long delayTime, Event [] eventList){
+            super(delayTime);
+            this.eventList = eventList;
+            for(Event e : eventList) {
+                addEvent(e);
+            }
+        }
+        public void action() {
+            for(Event e : eventList) {
+                e.start();// перезапуск каждого события
+                addEvent(e);
+            }
+        }
+    }
+
+}
+
