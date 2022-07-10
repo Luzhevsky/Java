416c45ad3a1bbef5d0a5bb338b55870d6b62571e
Dmitriy Luzhevsky <dmitriy8796@inbox.ru>
2022-07-10 23:47:07 +0300

    JavaPhilosophy Chapter11Collection TaskOne

--- JavaPhilosophy/src/com/Chapter11Collections/TaskOne/Gerbil.java
+++ JavaPhilosophy/src/com/Chapter11Collections/TaskOne/Gerbil.java
@@ -1,5 +1,7 @@
 package com.Chapter11Collections.TaskOne;
 
+import java.util.ArrayList;
+
 public class Gerbil { //песчанка
     int gerbilNumber;
     Gerbil (int gerbilNumber){
@@ -7,5 +9,17 @@ public class Gerbil { //песчанка
     }
     public void hop() { //прыгать
         System.out.println(gerbilNumber + " hello");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Gerbil> gerbil = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            gerbil.add(new Gerbil(i));
+        }
+        for (int i = 0; i < gerbil.size(); i++) {
+            gerbil.get(i).hop();
+        }
     }
 }
